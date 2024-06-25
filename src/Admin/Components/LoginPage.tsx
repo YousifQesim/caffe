d243e52b6288a966dca6984c2778e8cf623ce487
yusifqasim <yusifqasim22@gmail.com>
2024-06-25 14:19:46 +0300

    refactor: Improve LoginPage component by adding form validation, error handling, and redirect after successful login

--- src/Admin/Components/LoginPage.tsx
+++ src/Admin/Components/LoginPage.tsx
@@ -2,38 +2,47 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useFormik } from 'formik';
-import {loginValidationSchema} from '../../validation/ValidationSchemas';
+import { loginValidationSchema } from '../../validation/ValidationSchemas';
 
+/**
+ * LoginPage component renders a form for user login.
+ * It handles form submission, validation using Formik, and redirects to '/admin' upon successful login.
+ */
 const LoginPage: React.FC = () => {
     const navigate = useNavigate();
-    const [error, setError] = useState('');
+    const [error, setError] = useState<string>(''); // State for displaying login errors
 
-  
+    // Formik hook for managing form state, validation, and submission
     const formik = useFormik({
         initialValues: {
             username: '',
             password: '',
         },
-        validationSchema: loginValidationSchema,
+        validationSchema: loginValidationSchema, // Validation schema for username and password
         onSubmit: async (values) => {
             try {
+                // Attempt to login via API
                 const response = await axios.post('http://localhost:3000/api/login', {
                     username: values.username,
                     password: values.password,
                 });
+                // Store token in localStorage upon successful login
                 localStorage.setItem('token', response.data.token);
+                // Redirect to '/admin' after successful login
                 navigate('/admin');
             } catch (err) {
+                // Handle login errors
                 setError('Invalid username or password');
             }
         },
     });
 
     return (
-        <div className="flex items-center justify-center min-h-screen ">
+        <div className="flex items-center justify-center min-h-screen">
             <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
                 <h2 className="text-2xl font-bold mb-6 text-center text-main">Login</h2>
                 <form onSubmit={formik.handleSubmit}>
+                    {/* Username input field */}
                     <div className="mb-4">
                         <label className="block text-main font-bold">Username:</label>
                         <input
@@ -46,13 +55,15 @@ const LoginPage: React.FC = () => {
                             className={`w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none ${
                                 formik.touched.username && formik.errors.username
                                     ? 'border-red-500'
-                                    : 'focus:border-main-2 focus:border-main-2'
+                                    : 'focus:border-main-2'
                             }`}
                         />
-                        {formik.touched.username && formik.errors.username ? (
+                        {/* Display username validation error */}
+                        {formik.touched.username && formik.errors.username && (
                             <div className="text-red-500 text-sm font-bold">{formik.errors.username}</div>
-                        ) : null}
+                        )}
                     </div>
+                    {/* Password input field */}
                     <div className="mb-4">
                         <label className="block text-main font-bold">Password:</label>
                         <input
@@ -65,17 +76,20 @@ const LoginPage: React.FC = () => {
                             className={`w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none ${
                                 formik.touched.password && formik.errors.password
                                     ? 'border-red-500'
-                                    : 'focus:border-main-2 focus:border-main-2'
+                                    : 'focus:border-main-2'
                             }`}
                         />
-                        {formik.touched.password && formik.errors.password ? (
+                        {/* Display password validation error */}
+                        {formik.touched.password && formik.errors.password && (
                             <div className="text-red-500 text-sm font-bold">{formik.errors.password}</div>
-                        ) : null}
+                        )}
                     </div>
+                    {/* Display login error if any */}
                     {error && <p className="text-red-600 mb-4 font-bold">{error}</p>}
+                    {/* Login button */}
                     <button
                         type="submit"
-                        className={`w-full bg-main text-white py-2 rounded-lg  focus:outline-none ${
+                        className={`w-full bg-main text-white py-2 rounded-lg focus:outline-none ${
                             formik.isValid ? 'focus:ring-2 focus:ring-blue-600' : 'opacity-50 cursor-not-allowed'
                         }`}
                         disabled={!formik.isValid}
